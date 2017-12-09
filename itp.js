4c00b380b6560d2c0385aafd4bc76b7364bd794d
Iikka Hauhio <iikka@kaivos.org>
2017-12-10 01:43:09 +0200

    added comments

--- itp.js
+++ itp.js
@@ -1,10 +1,17 @@
-Array.prototype.lisätä_N_T = function(kiva_arvo) { this.push(kiva_arvo); }
+// Array
+Array.prototype.lisätä_N_T = function(kiva_arvo) { this.push(kiva_arvo); };
 Array.prototype.f_määrä = function() { return this.length; };
 
+// Number/luku
 var power_handler = {get: (a, b) => Math.pow(a, Number.parseInt(b)+1)};
 Number.prototype.f_potenssi = function() { return new Proxy(this, power_handler); };
 
-HTMLDocument.prototype.näyttää_G_N = HTMLDocument.prototype.write;
+// HTMLDocument/sivu
+HTMLDocument.prototype.näyttää_G_N = function(x) { this.write(x+"\n"); };
 
-function muuttuja(vals) { this.arvo = ("arvo" in vals) ? vals["arvo"] : undefined; }; muuttuja.prototype.f_arvo = function() { return this.arvo; };
+// muuttuja
+function muuttuja(vals) {
+ this.arvo = ("arvo" in vals) ? vals["arvo"] : undefined;
+};
+muuttuja.prototype.f_arvo = function() { return this.arvo; };
 muuttuja.prototype.lukea_luku__St = function() { this.arvo = Number.parseInt(prompt("Syötä luku")); };
