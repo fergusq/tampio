6a3b07edb7c86e5beeae0431b50211ed7d450071
Iikka Hauhio <iikka@kaivos.org>
2017-12-10 23:59:38 +0200

    added square root function

--- itp.js
+++ itp.js
@@ -5,6 +5,7 @@ Array.prototype.f_määrä = function() { return this.length; };
 // Number/luku
 var power_handler = {get: (a, b) => Math.pow(a, Number.parseInt(b)+1)};
 Number.prototype.f_potenssi = function() { return new Proxy(this, power_handler); };
+Number.prototype.f_neliöjuuri = function() { return Math.sqrt(this); };
 
 // HTMLDocument/sivu
 HTMLDocument.prototype.näyttää_G_N = function(x) { this.write(x+"\n"); };
