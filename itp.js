b1bc2dd210a974da47048a4b247d7079faca4adc
Iikka Hauhio <iikka@kaivos.org>
2017-12-11 19:09:04 +0200

    date library

--- itp.js
+++ itp.js
@@ -13,6 +13,19 @@ String.prototype.f_pituus = function() { return this.length; };
 // HTMLDocument/sivu
 HTMLDocument.prototype.näyttää_G_N = function(x) { this.write(x); };
 
+// Date
+Date.prototype.f_päivämäärämerkkijono_E = function() { return this.getLocaleDateString(); };
+Date.prototype.f_kellonaikamerkkijono_E = function() { return this.getLocaleTimeString(); };
+Date.prototype.f_merkkijono_E = function() { return this.getLocaleString(); };
+Date.prototype.f_millisekunti_E = function() { return this.getTime(); };
+Date.prototype.f_vuosi = function() { return this.getFullYear(); };
+Date.prototype.f_kuukausi = function() { return this.getMonth(); };
+Date.prototype.f_päivä = function() { return this.getDay(); };
+Date.prototype.f_tunti = function() { return this.getHours(); };
+Date.prototype.f_minuutti = function() { return this.getMinutes(); };
+Date.prototype.f_sekunti = function() { return this.getSeconds(); };
+var nykyinen_aika = new Date();
+
 // muuttuja
 function muuttuja(vals) {
  this.arvo = ("arvo" in vals) ? vals["arvo"] : undefined;
